1d03941baba9cdc42e12ae8247705f4e2cf3bb03
Dennis Omoyibo <dennisegheosaomoyibo@gmail.com>
2026-03-28 23:32:36 +0100

    Todo list

---
+++ todo-list.js
@@ -0,0 +1,15 @@
+const taskInput = document.querySelector('.input.js');
+const addButton = document.querySelector('.add-task-btn-js');
+
+const tasks = [];
+
+function AddTask() {
+  const taskText = taskInput.value;
+
+if (taskText.value === "") {
+alert('Please enter a task!');
+return;
+}
+}
+
+
